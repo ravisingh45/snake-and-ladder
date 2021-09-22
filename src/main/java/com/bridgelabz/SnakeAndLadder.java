e2bcdf25ac52b897f60b6d68bfd18165bd4b28df
ravisingh45@gmail.com <81140145+ravisingh45@users.noreply.github.com>
2021-09-22 16:41:15 +0530

    ADDED:Checks for a option

--- src/main/java/com/bridgelabz/SnakeAndLadder.java
+++ src/main/java/com/bridgelabz/SnakeAndLadder.java
@@ -1,11 +1,32 @@
 package com.bridgelabz;
 
 public class SnakeAndLadder {
+      public static  int player = 0;
     public static void main(String[] args) {
-        int player = 1;
-        int turn = (int)Math.floor(Math.random() * 10) % 6;
-        System.out.println("**************** Welcome to the Snake and ladder game****************");
-        System.out.println("Your player is at: "+turn+" position at the board");
+
+       int check = (int)Math.floor(Math.random() * 10) % 3;
+        int turn = (int)Math.floor(Math.random() * 6) + 1;
+        switch (check){
+            case 1:
+                System.out.println("ladder");
+                System.out.println("die value: "+turn);
+                player += turn;
+                System.out.println("player is in position: " + player);
+                break;
+            case 2:
+                System.out.println("Snake");
+                System.out.println("die value: "+turn);
+                System.out.println("player is in position: " + player);
+                player += turn;
+                break;
+            case 3:
+                System.out.println("No play");
+                System.out.println(turn);
+                System.out.println("player is in position: " + player);
+                break;
+            default:
+                System.out.println("something Happen sorry !!");
+        }
 
     }
 }
