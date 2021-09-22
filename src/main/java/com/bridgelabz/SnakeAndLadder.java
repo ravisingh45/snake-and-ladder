7a1192febe974ef85fb6b4383f0f0e5e7ba94073
ravisingh45@gmail.com <81140145+ravisingh45@users.noreply.github.com>
2021-09-23 02:34:52 +0530

    ADDED:Player win when he reache the position 100

--- src/main/java/com/bridgelabz/SnakeAndLadder.java
+++ src/main/java/com/bridgelabz/SnakeAndLadder.java
@@ -1,31 +1,66 @@
 package com.bridgelabz;
 
 public class SnakeAndLadder {
-      public static  int player = 0;
-    public static void main(String[] args) {
+    //CONSTANT
+    static int POSITION_OF_PLAYER = 0;
+    static int START_POSITION = 0;
+    static int END_POSITION = 100;
 
-       int check = (int)Math.floor(Math.random() * 10) % 3;
-        int turn = (int)Math.floor(Math.random() * 6) + 1;
-        switch (check){
-            case 1:
-                System.out.println("ladder");
-                System.out.println("die value: "+turn);
-                player += turn;
-                System.out.println("player is in position: " + player);
-                break;
-            case 2:
-                System.out.println("Snake");
-                System.out.println("die value: "+turn);
-                System.out.println("player is in position: " + player);
-                player += turn;
-                break;
-            case 3:
-                System.out.println("No play");
-                System.out.println(turn);
-                System.out.println("player is in position: " + player);
-                break;
-            default:
-                System.out.println("something Happen sorry !!");
+    public static void main(String[] args) {
+        // Play the game till the end position
+        while (POSITION_OF_PLAYER != END_POSITION) {
+            // Generate random value
+            int RANDOM_DIE_VALUE = (int) (Math.random() * 6) + 1;
+            int RANDOM_CHECK = (int) (Math.random() * 10) % 3;
+            // Check the condition
+            switch (RANDOM_CHECK) {
+                case 1:
+                    System.out.println("Ladder");
+                    System.out.println("Dice value: " + RANDOM_DIE_VALUE);
+                    POSITION_OF_PLAYER += RANDOM_DIE_VALUE;
+                    System.out.println("Player position is: " + POSITION_OF_PLAYER);
+                    if (POSITION_OF_PLAYER > END_POSITION) {
+                        POSITION_OF_PLAYER = END_POSITION;
+                        System.out.println("player Position: " + POSITION_OF_PLAYER);
+                        System.out.println("Player Won !!!");
+                        break;
+                    }
+
+                    System.out.println("player Position: " + POSITION_OF_PLAYER);
+                    System.out.println("");
+                    break;
+
+                case 2:
+                    System.out.println("Snake");
+                    System.out.println("Dice value: " + RANDOM_DIE_VALUE);
+                    POSITION_OF_PLAYER -= RANDOM_DIE_VALUE;
+                    System.out.println("Player position is: " + POSITION_OF_PLAYER);
+                    if (POSITION_OF_PLAYER < START_POSITION) {
+                        POSITION_OF_PLAYER = START_POSITION;
+                        System.out.println("player Position: " + POSITION_OF_PLAYER);
+                    }
+
+                    System.out.println("player Position: " + POSITION_OF_PLAYER);
+                    System.out.println("");
+                    break;
+
+                case 0:
+                    System.out.println("No Play");
+                    POSITION_OF_PLAYER = POSITION_OF_PLAYER;
+                    System.out.println("Player position is: " + POSITION_OF_PLAYER);
+                    System.out.println("");
+                    break;
+
+                default:
+                    System.out.println("Something Unexpectated Happen !!");
+
+            }
+
+            // Condition check for player position
+            if (POSITION_OF_PLAYER < START_POSITION) {
+                POSITION_OF_PLAYER = START_POSITION;
+            }
+
         }
 
     }
