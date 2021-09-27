5741885a42781a7c358779da1b5174098529b412
ravisingh45@gmail.com <81140145+ravisingh45@users.noreply.github.com>
2021-09-27 19:32:27 +0530

    ADDED:The plear will win in exact 100 position

--- src/main/java/com/bridgelabz/SnakeAndLadder.java
+++ src/main/java/com/bridgelabz/SnakeAndLadder.java
@@ -7,11 +7,14 @@ public class SnakeAndLadder {
     static int END_POSITION = 100;
 
     public static void main(String[] args) {
+
         // Play the game till the end position
         while (POSITION_OF_PLAYER != END_POSITION) {
+
             // Generate random value
             int RANDOM_DIE_VALUE = (int) (Math.random() * 6) + 1;
             int RANDOM_CHECK = (int) (Math.random() * 10) % 3;
+
             // Check the condition
             switch (RANDOM_CHECK) {
                 case 1:
@@ -20,12 +23,13 @@ public class SnakeAndLadder {
                     POSITION_OF_PLAYER += RANDOM_DIE_VALUE;
                     System.out.println("Player position is: " + POSITION_OF_PLAYER);
                     if (POSITION_OF_PLAYER > END_POSITION) {
-                        POSITION_OF_PLAYER = END_POSITION;
-                        System.out.println("player Position: " + POSITION_OF_PLAYER);
+                        POSITION_OF_PLAYER -= RANDOM_DIE_VALUE;
+                    }
+                    else if (POSITION_OF_PLAYER == END_POSITION) {
+                        System.out.println("Player position is: " + POSITION_OF_PLAYER);
                         System.out.println("Player Won !!!");
                         break;
                     }
-
                     System.out.println("player Position: " + POSITION_OF_PLAYER);
                     System.out.println("");
                     break;
@@ -34,12 +38,10 @@ public class SnakeAndLadder {
                     System.out.println("Snake");
                     System.out.println("Dice value: " + RANDOM_DIE_VALUE);
                     POSITION_OF_PLAYER -= RANDOM_DIE_VALUE;
-                    System.out.println("Player position is: " + POSITION_OF_PLAYER);
                     if (POSITION_OF_PLAYER < START_POSITION) {
                         POSITION_OF_PLAYER = START_POSITION;
                         System.out.println("player Position: " + POSITION_OF_PLAYER);
                     }
-
                     System.out.println("player Position: " + POSITION_OF_PLAYER);
                     System.out.println("");
                     break;
@@ -56,12 +58,8 @@ public class SnakeAndLadder {
 
             }
 
-            // Condition check for player position
-            if (POSITION_OF_PLAYER < START_POSITION) {
-                POSITION_OF_PLAYER = START_POSITION;
-            }
-
         }
 
     }
+
 }
